a21c27a051052c8e5c31ca072336a9ae284496d6
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-30 14:33:04 +0530

    allowing some specific like /welcome /about /carrers

---
+++ Day1SEcurity/src/main/java/com/navi8/Day1SEcurity/Controller/MsgRestController.java
@@ -0,0 +1,18 @@
+package com.navi8.Day1SEcurity.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MsgRestController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome to Naveen world..!!";
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Good Morning..!!";
+    }
+}
---
+++ Day1SEcurity/src/test/java/com/navi8/Day1SEcurity/Day1SEcurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi8.Day1SEcurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1SEcurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
