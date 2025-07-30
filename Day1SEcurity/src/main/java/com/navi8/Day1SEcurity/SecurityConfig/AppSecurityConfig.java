f286b12f8911bcf3e6edd14e55f78c33d3b9d700
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-30 15:29:19 +0530

    user creationg but not recom only for practice

--- Day1SEcurity/src/main/java/com/navi8/Day1SEcurity/SecurityConfig/AppSecurityConfig.java
+++ Day1SEcurity/src/main/java/com/navi8/Day1SEcurity/SecurityConfig/AppSecurityConfig.java
@@ -5,12 +5,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig {
       //security filter chain **interface**
+	
+	
+	@Bean
+	public InMemoryUserDetailsManager inMemoryUsers() {
+
+	    UserDetails u1 = User.withDefaultPasswordEncoder()
+	        .username("ashokit")
+	        .password("ashokit@123")
+	        .build();
+
+	    UserDetails u2 = User.withDefaultPasswordEncoder()
+	        .username("raju")
+	        .password("raju@123")
+	        .build();
+
+	    return new InMemoryUserDetailsManager(u1, u2);
+	}
+
+	
+	
+	
+	
+	
 	
 	@Bean
 	public SecurityFilterChain securityFilteConfig(HttpSecurity http) throws Exception
